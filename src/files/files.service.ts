cd494e84e65ab41990721a255381bebdfd878d5d
Temirbaev Temirlan <temirlan213@bk.ru>
2023-10-09 08:48:51 +0600

    test7

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -13,7 +13,7 @@ export class FilesService {
     //   fs.mkdirSync(filePath, {recursive: true})
     // }
 
-    // fs.writeFileSync(path.join(filePath, fileName), file.buffer);
+    fs.writeFileSync(path.join(filePath, fileName), file.buffer);
     return fileName
   }
 }
