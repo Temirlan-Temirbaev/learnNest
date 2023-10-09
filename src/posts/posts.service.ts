f54d73bd9cf8ca978d3d89e6cc32e6bd23c4e4ae
Temirbaev Temirlan <temirlan213@bk.ru>
2023-10-09 08:12:46 +0600

    test1

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -12,8 +12,8 @@ export class PostsService {
   }
 
   async createPost(dto: CreatePostDto, image: any) {
-    const fileName: string = await this.fileService.createFile(image);
-    const post = await this.postRepository.create({...dto, userId: Number(dto.userId), image: fileName})
+    // const fileName: string = await this.fileService.createFile(image);
+    const post = await this.postRepository.create({...dto, userId: Number(dto.userId), image: ""})
 
     return post;
   }
